7c999423e7cae5f2168be2f0123a7a066f3252b7
Yunian Edwi Risnawa <100954182+yunpnzr@users.noreply.github.com>
2025-02-23 14:32:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const jobs = require("./jobs");
 
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("App successful")
-})
-
 app.listen(port, () => {
   console.log(`App Running On : http://localhost:${port}`);
   jobs()
